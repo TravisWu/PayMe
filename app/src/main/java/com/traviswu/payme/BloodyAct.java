f00d114927f5f7b5283fe2613802096a371ff3a4
traviswu <travis.wu@queensu.ca>
2015-06-03 23:42:29 -0700

    added button with new class BloodyAct, will complete it tomorrow

---
+++ app/src/main/java/com/traviswu/payme/BloodyAct.java
@@ -0,0 +1,17 @@
+package com.traviswu.payme;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by traviswu on 2015-06-03.
+ */
+public class BloodyAct extends ActionBarActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bloodymarry);
+    }
+
+
+}
