5199aceae443b1cba3885934e38472732d06fe65
tanvi <14tp17@queensu.ca>
2015-06-03 21:03:24 -0400

    Implemented fragments to make the swipe screen feature work. Added new classes namely: FragmentOne, PagerAdapter, SharkTank, Added new xml layouts as well. Made changes to the MainActivity to make it compatible with the fragment architecture. Also, connected the Shark Split button back to the SharkSplit class.

--- app/src/main/java/com/traviswu/payme/MainActivity.java
+++ app/src/main/java/com/traviswu/payme/MainActivity.java
@@ -1,26 +1,22 @@
 package com.traviswu.payme;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v4.view.ViewPager;
+import android.support.v4.app.FragmentActivity;
+
 
 /**
  * Created by traviswu on 2015-05-30.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
+    ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-
-        Button sharksplit = (Button) findViewById(R.id.sharkSplit);
-            sharksplit.setOnClickListener(new Button.OnClickListener(){
-                public void onClick(View v){
-                    startActivity(new Intent(MainActivity.this, SharkSplit.class ));
-                }
-            });
-
+        setContentView(R.layout.main_activity);
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        PagerAdapter padapter = new PagerAdapter(getSupportFragmentManager());
+        viewPager. setAdapter(padapter);
     }
 }
---
+++ app/src/main/java/com/traviswu/payme/SharkTank.java
@@ -0,0 +1,19 @@
+package com.traviswu.payme;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by User on 02-06-2015.
+ */
+public class SharkTank extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        return inflater.inflate(R.layout.shark_tank,container,false);
+    }
+}
