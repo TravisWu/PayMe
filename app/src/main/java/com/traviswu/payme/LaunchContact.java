05d780f57a00fbba43a7014df5c06096a4d03bca
Xianyang Qiao <12xq4@queensu.ca>
2015-06-09 11:33:36 -0600

    Merge branch 'master' of https://github.com/TravisWu/PayMe
    Conflicts:
    	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/traviswu/payme/LaunchContact.java
+++ app/src/main/java/com/traviswu/payme/LaunchContact.java
@@ -1,13 +1,24 @@
 package com.traviswu.payme;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.util.Iterator;
+import java.util.Set;
 
 
 public class LaunchContact extends ActionBarActivity {
 
+    private static final int CONTACT_PICKER_RESULT = 1001;
+    private static final String DEBUG_TAG = "From LaunchContact";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,5 +47,46 @@ public class LaunchContact extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void launchContactPicker(View view) {
+        Intent contactIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        contactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+        startActivityForResult(contactIntent, CONTACT_PICKER_RESULT);
+    }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case CONTACT_PICKER_RESULT:
+                    //handle contact result
+                    Bundle extras = data.getExtras();
+                    Set keys = extras.keySet();
+                    Iterator<String> iter = keys.iterator();
+                    while (iter.hasNext()) {
+                        String key = iter.next();
+                        Log.v(DEBUG_TAG, key + "[" + extras.get(key) + "]");
+                    }
+                    Uri result = data.getData();
+                    Log.v(DEBUG_TAG, "got result: " + result.toString());
+
+                    //grab the id from URI
+                    String id = result.getLastPathSegment();
+
+                    Uri newUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+                    String[] projection = new String[]{ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER};
+                    Cursor people = getContentResolver().query(newUri, projection, null, null, null);
+
+                    int indexName = people.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+                    int indexNum = people.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+
+                    people.moveToFirst();
+                    String[] columns = people.getColumnNames();
+
+                    break;
+
+            }
+        } else {
+            //wait to handle failure
+            Log.w(DEBUG_TAG, "Warning: activity result not ok");
+        }
+    }
 }
